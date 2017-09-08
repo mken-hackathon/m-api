06c413465e8a67efaa5bf62d941943245cd700ec
nishikawa_sh <nishikawa_sh@mti.co.jp>
2017-09-08 14:38:47 +0900

    ADD: アクティビティの一覧を取得するAPI

--- src/handler/activities.js
+++ src/handler/activities.js
@@ -1,8 +1,16 @@
+"use strict";
+
 const {handlers, HttpResponse} = require("./lambda");
+const Common = require("../common");
+const Db = require("../db");
 
 module.exports.get = handlers(function* (ev) {
+  const r = yield Db.Activities.gets();
   return new HttpResponse({
     statusCode: 200,
-    body: { message: "OK" }
+    body: { 
+      message: "OK" ,
+      activities: r
+    }
   })
 });
