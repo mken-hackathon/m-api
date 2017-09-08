a3c352357253b0f88af333b40a794816f16b9a68
nishikawa_sh <nishikawa_sh@mti.co.jp>
2017-09-08 14:05:49 +0900

    ADD: エンドポイント上からアクティビティを登録する

--- src/handler/buttonAction.js
+++ src/handler/buttonAction.js
@@ -1,8 +1,42 @@
+"use strict";
+
 const {handlers, HttpResponse} = require("./lambda");
+const Common = require("../common");
+const Db = require("../db");
+const EV = new Common.EventValidator({
+  Schema: {
+    id: "/Event",
+    type: "object",
+    required: ["pathParameters"],
+    properties: {
+      pathParameters: {
+        type: "object",
+        required: ["bid"],
+        properties: {
+          bid: {
+            type: "string"
+          }
+        }
+      }
+    }
+  }
+});
+
 
 module.exports.post = handlers(function* (ev) {
+  EV.validate(ev);
+
+  const bid = ev.pathParameters.bid;
+  const preset = yield Db.Presets.get(bid);
+  if(!preset) {
+    throw new Common.Error.NotFoundError("button not found");
+  }
+  yield Db.Activities.puts(preset.activities);
   return new HttpResponse({
     statusCode: 200,
-    body: { message: "OK" }
+    body: { 
+      message: "OK",
+      put: preset.activities
+    }
   })
 });
